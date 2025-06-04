d1c1364fad7e384f9f8a749ea4c6eee3034762a0
Nir Graziani <nirg6888@gmail.com>
2025-06-04 15:05:04 +0300

    Fixed countryList comp

--- src/sandbox/CountryList.jsx
+++ src/sandbox/CountryList.jsx
@@ -1,28 +1,47 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 function CardsPage() {
+  const [allCountries, setAllCountries] = useState([]);
   const [countries, setCountries] = useState([]);
 
 
-  const getCardsFromServer = async () => {
+  const getCountries = async () => {
     const respone = await fetch("https://restcountries.com/v3.1/all")
     const json = await respone.json();
-    setCountries(json.slice(0, 10));
+    setAllCountries(json.slice(0, 20));
+    setCountries(json.slice(0, 20));
 }
 
   useEffect(() => {
-    getCardsFromServer()
+    getCountries()
   }, []);
 
+  const handleInputChange = (event) => {
+    const value = event.target.value.toLowerCase();
+    const filtered = allCountries.filter((country) => {
+      return country.name.common.toLowerCase().includes(value)
+    })
+      setCountries(filtered);
+  }
+
   return (
-    <div>
-    {countries.map((country) => {
+    <Box sx={{display: 'flex', gap: "10px"}}>
+      <Box>
+        {countries.map((country) => {
     return (
-        <Box>
-            {country.name.common}
+      <Box key={country.name.common}>
+        <Typography>
+          {country.name.common}
+        </Typography>
+        <img src={country.flags.png}/>
         </Box>           
-    )})}
-    </div>
+      )
+    })}
+      </Box>
+      <Box >
+        <TextField onChange={handleInputChange} id="outlined-basic" label="Outlined" variant="outlined" />
+      </Box>
+    </Box>
   );
 }
 
