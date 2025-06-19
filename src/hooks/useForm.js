9b0d48d77109d2d6cb0442917f2da557fcfd5828
Nir Graziani <nirg6888@gmail.com>
2025-06-19 12:25:36 +0300

    Added joi browser

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,49 +1,49 @@
 import { useState } from "react";
-import Joi from "joi";
+import Joi from "joi"
 
 
 
-export default function  useForm(initialForm, validationSchema) {
-     const [formDetails, setUserDetails] = useState(initialForm);
-     const  [errors, setErrors] = useState({});
+export default function useForm(initialForm, validationSchema) {
+    const [formDetails, setUserDetails] = useState(initialForm);
+    const [errors, setErrors] = useState({});
 
     const fullSchema = Joi.object(validationSchema);
-    
 
-        const handleChange = (e) => {
-            const fieldName = e.target.name
-            const fieldValue = e.target.value
-    
-            setUserDetails((prev) => ({
-                ...prev,
-                [fieldName]: fieldValue,
-            }))
-            
+
+    const handleChange = (e) => {
+        const fieldName = e.target.name
+        const fieldValue = e.target.value
+
+        setUserDetails((prev) => ({
+            ...prev,
+            [fieldName]: fieldValue,
+        }))
+
         const updatedDetails = {
-                ...formDetails,
-                [fieldName]: fieldValue
-            };
-    
-            const { error } = fullSchema.validate(updatedDetails, { abortEarly: false })
-            console.log(error)
-            if (error) {
-                const fieldErrors = {};
-                error.details.forEach(err => {
-                    fieldErrors[err.path[0]] = err.message;
-                })
-                setErrors(fieldErrors);
-            } else {
-                setErrors({})
-            }
-            
+            ...formDetails,
+            [fieldName]: fieldValue
+        };
+
+        const { error } = fullSchema.validate(updatedDetails, { abortEarly: false })
+        console.log(error)
+        if (error) {
+            const fieldErrors = {};
+            error.details.forEach(err => {
+                fieldErrors[err.path[0]] = err.message;
+            })
+            setErrors(fieldErrors);
+        } else {
+            setErrors({})
         }
-    
-        const handleSubmit = () => {
-            console.log(formDetails)
-            const {error} = fullSchema.validate(formDetails, {abortEarly: false})
-            console.log(error)
-        }
-    
+
+    }
+
+    const handleSubmit = () => {
+        console.log(formDetails)
+        const { error } = fullSchema.validate(formDetails, { abortEarly: false })
+        console.log(error)
+    }
+
     return {
         formDetails,
         errors,
